28ed304df7938fdaa5045a4284d636119489b25f
tuiordan <tiordan@bellarmine.edu>
2024-04-10 13:41:55 -0400

    first commit

---
+++ Assignment_7.2/gitDemo.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+
+/**
+ * @author Tudor Iordan
+ * 
+ */
+public class gitDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello Dr. Johnson! This is Tudor Iordan.");
+	}
+
+}
